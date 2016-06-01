4d23f4bf457e33b1b9ea33ff5ab0a5bb8498aa8f
infratabgrandhis <infratab.grandhis@gmail.com>
2016-06-01 18:36:14 +0530

    making openWeather call to get weather data.

--- openWeather/routes/index.js
+++ openWeather/routes/index.js
@@ -1,12 +1,13 @@
 var express = require('express'),
     router = express.Router(),
-    validation=require("../validation")();
+    validation=require("../validation")(),
+    services=require("../services")();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/weather', validation.cityValidation);
+router.get('/weather', validation.cityValidation,services.getWeatherData);
 
 module.exports = router;
---
+++ openWeather/services/index.js
@@ -0,0 +1,28 @@
+var q=require("q"),
+    request=require("superagent"),
+    querystring=require("querystring");
+
+function getWeatherData(req,res,next){
+    var cityName=req.query.city,
+        openWhether=process.envObj.openWhether,
+        queryObj={
+            "q": cityName,
+            "appid":openWhether.apiKey
+        },
+        url=openWhether.hostName+"?"+querystring.stringify(queryObj);
+    request
+        .get(url)
+        .end(function(err, result){
+            if(err){
+                res.status(200).send(err);
+            }else{
+                res.status(200).send(result.body);
+            }
+        });
+}
+
+module.exports=function(){
+    return {
+        getWeatherData:getWeatherData
+    }
+}
